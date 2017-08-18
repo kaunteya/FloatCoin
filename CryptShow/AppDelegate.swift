f2ea2c18448c2f5217d8ac248968520c72872a96
Kaunteya Suryawanshi <kaunteya@kaunteys-macbook-pro.local>
2017-08-18 22:58:32 +0530

    Initial Commit

---
+++ CryptShow/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  CryptShow
+//
+//  Created by Kaunteya Suryawanshi on 18/08/17.
+//  Copyright © 2017 Kaunteya Suryawanshi. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
