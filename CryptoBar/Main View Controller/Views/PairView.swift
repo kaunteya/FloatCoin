7f437cc58a7608edd6eb8f5ae7e6d64e4d3ac1be
Kaunteya Suryawanshi <kaunteya@kaunteys-macbook-pro.local>
2017-12-30 17:14:00 +0530

    ,

--- CryptoBar/Main View Controller/Views/PairView.swift
+++ CryptoBar/Main View Controller/Views/PairView.swift
@@ -1,13 +1,9 @@
 //
-//  OutlineVC.swift
+//  PairView.swift
 //  FloatCoin
 //
 //  Created by Kaunteya Suryawanshi on 30/12/17.
 //  Copyright © 2017 Kaunteya Suryawanshi. All rights reserved.
 //
 
-import AppKit
-
-class OutlineVC: NSViewController {
-    
-}
+import Foundation
