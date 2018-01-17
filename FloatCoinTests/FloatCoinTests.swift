b0e883259cde30ea000af5614f5a9b2ee574aa00
Kaunteya Suryawanshi <kaunteya@kaunteys-macbook-pro.local>
2018-01-17 18:00:00 +0530

    Test cases for user defaults

--- FloatCoinTests/FloatCoinTests.swift
+++ FloatCoinTests/FloatCoinTests.swift
@@ -10,5 +10,18 @@ import XCTest
 
 
 class FloatCoinTests: XCTestCase {
+    let defaults = UserDefaults.standard
 
+    let testKey = "testSetOnce"
+
+    override func tearDown() {
+        defaults.removeObject(forKey: testKey)
+    }
+
+    func testUserDefaultsSetOnce() {
+        defaults.setOnce(1, forKey: testKey)
+        XCTAssertEqual(defaults.integer(forKey: testKey), 1)
+        defaults.setOnce(10, forKey: testKey)
+        XCTAssertEqual(defaults.integer(forKey: testKey), 1)
+    }
 }
