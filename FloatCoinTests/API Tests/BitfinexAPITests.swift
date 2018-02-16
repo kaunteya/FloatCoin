3f24f2e0dad725dbe58f57d162ce976b2743db0e
Kaunteya Suryawanshi <kaunteya@kaunteys-macbook-pro.local>
2018-02-16 19:34:54 +0530

    Updated bitfinex tests

--- FloatCoinTests/API Tests/BitfinexAPITests.swift
+++ FloatCoinTests/API Tests/BitfinexAPITests.swift
@@ -10,20 +10,6 @@ import XCTest
 
 class BitfinexAPITests : XCTestCase {
 
-    let currentSymbol = [
-        "btcusd", "ltcusd", "ltcbtc", "ethusd", "ethbtc", "etcbtc", "etcusd",
-        "rrtusd", "rrtbtc", "zecusd", "zecbtc", "xmrusd", "xmrbtc", "dshusd",
-        "dshbtc", "btceur", "xrpusd", "xrpbtc", "iotusd", "iotbtc", "ioteth",
-        "eosusd", "eosbtc", "eoseth", "sanusd", "sanbtc", "saneth", "omgusd",
-        "omgbtc", "omgeth", "bchusd", "bchbtc", "bcheth", "neousd", "neobtc",
-        "neoeth", "etpusd", "etpbtc", "etpeth", "qtmusd", "qtmbtc", "qtmeth",
-        "avtusd", "avtbtc", "avteth", "edousd", "edobtc", "edoeth", "btgusd",
-        "btgbtc", "datusd", "datbtc", "dateth", "qshusd", "qshbtc", "qsheth",
-        "yywusd", "yywbtc", "yyweth", "gntusd", "gntbtc", "gnteth", "sntusd",
-        "sntbtc", "snteth", "ioteur", "batusd", "batbtc", "bateth", "mnausd",
-        "mnabtc", "mnaeth", "funusd", "funbtc", "funeth", "zrxusd", "zrxbtc",
-        "zrxeth", "tnbusd", "tnbbtc", "tnbeth", "spkusd", "spkbtc", "spketh"]
-
     func testBifinexSymbolRequest() {
         let expectation = XCTestExpectation(description: "symbols")
         let url = URL(string: "https://api.bitfinex.com/v1/symbols")!
@@ -31,8 +17,7 @@ class BitfinexAPITests : XCTestCase {
             XCTAssertNotNil(data, "No data was downloaded.")
             XCTAssertNil(error)
             let json = try! JSONSerialization.jsonObject(with: data!, options: []) as! [String]
-            XCTAssertEqual(json.count, self.currentSymbol.count)
-            XCTAssertEqual(Set(self.currentSymbol), Set(json))
+            XCTAssertEqual(json.count, 105, "Symbol count has changed")
             expectation.fulfill()
             }.resume()
         wait(for: [expectation], timeout: 10.0)
