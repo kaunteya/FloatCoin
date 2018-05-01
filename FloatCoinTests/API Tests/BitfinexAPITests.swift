1a18b02be8002f1c11ccd4449bbaa86384f11b44
Kaunteya Suryawanshi <k.suryawanshi@gmail.com>
2018-05-01 18:21:03 +0530

    Updated binance API tests

--- FloatCoinTests/API Tests/BitfinexAPITests.swift
+++ FloatCoinTests/API Tests/BitfinexAPITests.swift
@@ -13,14 +13,58 @@ class BitfinexAPITests : XCTestCase {
     func testPairCount() {
         let expectation = XCTestExpectation(description: "symbols")
         let url = URL(string: "https://api.bitfinex.com/v1/symbols")!
+
+        let currentPairs = Bitfinex.baseCurrencies.reduce(Set<String>()) { (result, currency) -> Set<String> in
+            let fiatlist = Bitfinex.FIATCurriences(crypto: currency)
+            let added = fiatlist.map { currency.stringValue.lowercased() + $0.stringValue.lowercased()}
+            return result.union(added)
+        }
+
         URLSession.shared.dataTask(with: url) { (data, response, error) in
             XCTAssertNotNil(data, "No data was downloaded.")
             XCTAssertNil(error)
-            let json = try! JSONSerialization.jsonObject(with: data!, options: []) as! [String]
-            XCTAssertEqual(json.count, 105, "Symbol count has changed")
+            let jsonPairs = try! JSONSerialization.jsonObject(with: data!, options: []) as! [String]
+            jsonPairs.forEach { XCTAssert($0.count == 6, "Not every pair is of form ABC:XYZ") }
+
+            let addedPairs = Set(jsonPairs).subtracting(currentPairs)
+            if addedPairs.count > 0 {
+                self.printDetails(Set(jsonPairs).splitPairsToDictionary())
+                XCTFail("Pairs added, update the list")
+            }
+
+            let removedPairs = currentPairs.subtracting(Set(jsonPairs))
+            if removedPairs.count > 0 {
+                self.printDetails(removedPairs.splitPairsToDictionary())
+                XCTFail("Pairs REMOVED")
+            }
+
             expectation.fulfill()
             }.resume()
         wait(for: [expectation], timeout: 10.0)
     }
+    func printDetails(_ dict: [String: Set<String>]) {
+        let sortedKeys = Array(dict.keys).sorted(by: { $0 < $1 })
+        print(sortedKeys.map { $0.uppercased()})
+        for a in sortedKeys {
+            let vals = dict[a]!.map { $0.uppercased()}
+            print("\"\(a.uppercased())\" : \(vals),")
+        }
+    }
 
+}
+
+fileprivate extension Set where Element == String {
+    func splitPairsToDictionary() -> [String: Set<String>]{
+        var dict = [String: Set<String>]()
+        for pair in self {
+            let three = pair.index(pair.startIndex, offsetBy: 3)
+            let (first,second) = (String(pair[..<three]), String(pair[three...]))
+            if dict[first] == nil {
+                dict[first] = Set(arrayLiteral: second)
+            } else {
+                dict[first] = dict[first]!.union([second])
+            }
+        }
+        return dict
+    }
 }
